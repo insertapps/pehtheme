4125a99d78680bdaa230e23e2f68310ab5a339db
Fauzan <bangpozan@gmail.com>
2024-01-12 17:24:10 +0700

    Sixth setup: replace '_S_' to 'PEHTHEME_'

--- functions.php
+++ functions.php
@@ -7,9 +7,9 @@
  * @package Pehtheme
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if ( ! defined( 'PEHTHEME_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define( 'PEHTHEME_VERSION', '1.0.0' );
 }
 
 /**
@@ -138,10 +138,10 @@ add_action( 'widgets_init', 'pehtheme_widgets_init' );
  * Enqueue scripts and styles.
  */
 function pehtheme_scripts() {
-	wp_enqueue_style( 'pehtheme-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'pehtheme-style', get_stylesheet_uri(), array(), PEHTHEME_VERSION );
 	wp_style_add_data( 'pehtheme-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'pehtheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'pehtheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), PEHTHEME_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
